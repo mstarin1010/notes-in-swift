d5118c718ff2d0bd0f3d054cb226a133781686c6
mstarin1010 <mstarin1010@gmail.com>
2020-09-24 17:13:48 -0400

    First commit

---
+++ notes-swift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  notes-swift
+//
+//  Created by mstarin on 9/24/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ notes-swift/notes_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  notes_swiftApp.swift
+//  notes-swift
+//
+//  Created by mstarin on 9/24/20.
+//
+
+import SwiftUI
+
+@main
+struct notes_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
